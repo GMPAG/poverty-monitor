4eb4eeebbe72817e74886e1ac4c8658ad2954fd8
Justin Hellings <justin.hellings@gmail.com>
2016-04-26 15:31:41 +0100

    Multi-iteration visualisation fully implemented.

--- indicator-visualisations/visualisations.js
+++ indicator-visualisations/visualisations.js
@@ -21,40 +21,29 @@ if ( ! console ) {
     };
 }
 
-function createChart( dataset )
+function recreateChart( indicator )
 {
+    // Destroy any previous chart.
+    if ( chart ) {
+        chart.destroy();
+        delete chart;
+    }
+
     var x_axis_id = 'geoname';
     var x_axis_title = 'Local authority';
     var chart_detail_level = DetailLevel.LA;
 
-    function getChartColumns() {
-        var keys = dataset.latestIndicatorKeys( detail_level );
-        var indicators = keys.map( function(key) {
-            return dataset.indicator(key, chart_detail_level);
-        });
 
-        var columns = indicators.map( function(indicator) {
-            return [ indicator.title ].concat(
-                indicator.data.filter(function (value, index) {
-//                     return ! geoCodeInRange( DetailLevel.BIG, indicator.geoCodes[index] );
-                    return true; // Including all areas. Not refactoring yet.
-                })
-            );
-        });
+    var HACK = 0;
+    var columns = indicator.iterations.map( function(iteration) {
+        HACK++;
+        return [ iteration.title+HACK ].concat( iteration.values );
+    });
+    columns.unshift( [ x_axis_id ].concat( indicator.iterations[0].geoNames ));
+    console.debug(columns);
 
-        // Add the geographical names as the first column.
-        columns.unshift( [ x_axis_id ].concat(
-            indicators[0].geoNames.filter(function (name, index) {
-//                     return ! geoCodeInRange( DetailLevel.BIG, indicators[0].geoCodes[index] );
-                return true; // Including all areas. Not refactoring yet.
-            })
-        ));
 
-        return columns;
-    }
-    var columns = getChartColumns();
-
-    chart = c3.generate( {
+    var config = {
         bindto: '#chart',
         size: {
             height: 400
@@ -62,9 +51,7 @@ function createChart( dataset )
         data: {
             x: x_axis_id,
             columns: columns,
-            type: 'bar',
-            // Hide all columns except x and first data column.
-            hide: columns.slice(2).map( function(col) {return col[0];} )
+            type: 'bar'
         },
         axis: {
             x: {
@@ -84,7 +71,7 @@ function createChart( dataset )
             },
             y: {
                 label: {
-                    text: columns[1].unitsLabel,
+                    text: indicator.chartYAxisLabel,
                     position: 'outer-middle'
                 }
             }
@@ -92,18 +79,108 @@ function createChart( dataset )
         legend: {
             show: false
         },
-        color: {
-            pattern: ['#ddd']
+        color : {
+            pattern: [ '#009ec8', '#79bb59', '#cad401', '#00a5aa' ]
         }
-    } );
+    };
+
+    c3.generate(config);
 }
 
-function getMapSql( indicator )
+function recreateTable ( indicator )
+{
+    if ( table ) {
+        // This fn is in the API docs but console says it does not exist.
+        // table.destroy( true );
+        delete table;
+    }
+
+    // Get rid of any previous table.
+    jQuery('#table').empty();
+
+    // Establish the number of rows in the table.
+    var num_rows = indicator.iterations[0].values.length;
+
+    // Check for a dataset that we do not know how to handle.
+    // i.e. One with a different number of rows.
+    for ( var i=1; i<indicator.iterations.length; i++ ) {
+        if ( indicator.iterations[i].values.length != num_rows ) {
+
+            debugger;
+
+            console.error("\
+Iterations for the current indicator have different numbers of values!\n\
+They may be based on different goegraphical areas." );
+            console.error( indicator );
+
+            jQuery('#table').append("<p>\
+Iterations for the current indicator have different numbers of values!\
+They may be based on different goegraphical areas.</p>" );
+
+            return;
+        }
+    }
+
+    // Create an empty table.
+    jQuery('#table').append(
+        '<table cellpadding="0" cellspacing="0" border="0" class="display"></table>' );
+
+    // Generate the table data and metadata.
+
+    var config = {
+        // Order column by the most recent data.
+        order:[[ indicator.numIterations, "desc" ]]
+    };
+
+    config.data = indicator.iterations[0].geoNames.map( function ( geoname, index ) {
+            var values = indicator.iterations.map( function(iteration) {
+                var value = iteration.values[index];
+                // ASSUMPTION: Negative data is invalid. Our current data source
+                // cannot contain nulls, hence the use of negative numbers.
+                return value >= 0 ? value : "No valid data";
+            });
+        values.unshift(geoname);
+        return values;
+    } );
+
+    config.columns = indicator.iterations.map( function(iteration) {
+        return {
+            title : iteration.title,
+            // Data may contain strings that describe missing data points.
+            // Force sorting to work for numbers rather than words.
+            type : "num"
+        }
+    });
+    config.columns.unshift( { title : "Area" } );
+
+//     console.debug(config);
+
+    if ( num_rows <= 20 ) {
+        config.paging = false;         // Turn off paging
+        config.info = false;           // Turn off paging info
+        config.bFilter = false;        // Turn off search
+        config.pageLength = num_rows;  // Remove empty rows
+
+        // Colour the background of wider regional rows differently.
+        config.rowCallback = function( row, data, index ) {
+            if ( ['Greater Manchester','North West','England'].indexOf( data[0] ) != -1 ) {
+                jQuery( row ).addClass( 'region' ).removeClass( 'local-authority' );
+            }
+            else {
+                jQuery( row ).removeClass( 'region' ).addClass( 'local-authority' );
+            }
+        };
+    }
+
+    table = jQuery('#table table').dataTable( config );
+}
+
+function getMapSql( iteration, detail_level )
 {
     var sql = 'SELECT cartodb_id, geo_name, the_geom, the_geom_webmercator, '
-    + indicator.key + " as indicator, '" + indicator.unitsLabel + "' as units FROM "
-    + indicator.datasetName + '_with_' + indicator.detailLevel
-    + '_boundaries WHERE the_geom IS NOT NULL AND 0.0 <= ' + indicator.key;
+    + iteration.key + " as indicator, '" + iteration.unitsLabel + "' as units FROM "
+    + iteration.datasetName + '_with_' + detail_level
+    + '_boundaries WHERE the_geom IS NOT NULL AND 0.0 <= ' + iteration.key;
     console.debug(sql);
     return sql;
 }
@@ -186,16 +263,21 @@ text-placement-type: simple;   \n\
     return result;
 }
 
-function updateMapForMeasure( indicator )
+function updateMapForIndicator( indicator )
 {
+//     console.debug(indicator);
+    var iteration = indicator.latestIteration;
+
     jQuery('#map-label').text(indicator.mapLabel)
 
-    map.getLayers()[1].getSubLayers()[0].setSQL( getMapSql(indicator) );
+    map.getLayers()[1].getSubLayers()[0].setSQL(
+        getMapSql(iteration, indicator.detailLevel) );
 
-    // Remove big things from the data. They will not be shown on the map and
-    // should not affect the chloropleth.
-    var data = indicator.data.filter( function(val, index) {
-        return val >= 0  &&  ! geoCodeInRange( DetailLevel.BIG, indicator.geoCodes[index] );
+    // Remove negative values from the data. (They indicate invalid data.)
+    // Remove big things from the data. (They will not be shown on the map and
+    // should not affect the chloropleth.)
+    var data = iteration.values.filter( function(val, index) {
+        return val >= 0  &&  ! geoCodeInRange( DetailLevel.BIG, iteration.geoCodes[index] );
     });
     var min = Math.min.apply(null, data);
     var max = Math.max.apply(null, data);
@@ -211,36 +293,18 @@ function updateMapForMeasure( indicator )
         jQuery( '.cartodb-legend-stack .min' ).text( "Least deprived" );
         jQuery( '.cartodb-legend-stack .max' ).text( "Most deprived" );
     } else {
-        jQuery( '.cartodb-legend-stack .min' ).text( min + indicator.unitsLabel );
-        jQuery( '.cartodb-legend-stack .max' ).text( max + indicator.unitsLabel );
+        jQuery( '.cartodb-legend-stack .min' ).text( min +' '+ iteration.unitsLabel );
+        jQuery( '.cartodb-legend-stack .max' ).text( max +' '+ iteration.unitsLabel );
     }
 }
 
-function updateChartForMeasure( indicator )
-{
-    chart.hide();
-    chart.axis.labels( {
-        y: indicator.chartYAxisLabel ? indicator.chartYAxisLabel : indicator.unitsLabel
-    } );
-    chart.show( indicator.title );
-}
-
-//     function selectCategory( category )
-//     {
-//         jQuery( '#measure-selector .top-level li' ).removeClass( 'active' );
-//         jQuery( '#measure-selector .top-level [category="' + category + '"]' ).addClass( 'active' );
-
-//         jQuery( '#measure-selector .second-level ul' ).addClass( 'hidden' );
-//         jQuery( '#measure-selector .second-level .' + category ).removeClass( 'hidden' );
-//     }
-
-function updateSelectorForMeasure( indicator )
+function updateSelectorForIndicator( indicator )
 {
     jQuery( '#measure-selector li' ).removeClass( 'active' );
     jQuery( '#measure-selector li[measure="' + indicator.name + '"]' ).addClass( 'active' );
 }
 
-function updateLinksForMeasure( indicator )
+function updateLinksForIndicator( indicator )
 {
     jQuery( '#list-of-links > li > a' ).each( function () {
         var href = jQuery(this).attr('href');
@@ -267,24 +331,21 @@ function selectIndicator( indicator )
 
     jQuery( '#description-link' )
     .empty()
-    .append( '<p>Read more <a href="/poverty-monitor/indicator-descriptions/?name='
+    .append( '<p>For further information about this data set, please see the accompanying "\
+<a href="/poverty-monitor/indicator-descriptions/?name='
             + encodeURIComponent(indicator.title)
-            + '">about this indicator</a>.</p>'  );
+            + '">About this indicator</a>" page.</p>'  );
 
-    updateSelectorForMeasure( indicator );
-    if ( chart ) {
-        updateChartForMeasure( indicator );
+    updateSelectorForIndicator( indicator );
+    updateMapForIndicator( indicator );
+    updateLinksForIndicator( indicator );
+
+    recreateTable( indicator );
+
+    if ( detail_level == DetailLevel.LA ) {
+        recreateChart( indicator );
     }
-    updateMapForMeasure( indicator );
-    drawTable( indicator );
-
-    updateLinksForMeasure( indicator );
 }
-
-//     function onCategoryClicked(e)
-//     {
-//         selectCategory( jQuery(e.target).attr('category') );
-//     }
 
 function onMeasureClicked(e)
 {
@@ -293,124 +354,55 @@ function onMeasureClicked(e)
             jQuery(e.currentTarget).attr('measure'), detail_level ) );
 }
 
-function createSelector( dataset )
-{
-    dataset.latestIndicatorKeys(detail_level).forEach( function (key)
-                {
-                    //             jQuery( '#measure-selector .top-level li' ).click( onCategoryClicked );
-
-//                     var li_class = 'poverty-' + dataset.getIndicatorCategory(key);
-
-                    //             jQuery( '#measure-selector ul.' + li_class + 's' )
-                    jQuery( '#measure-selector ul' )
-                    .append(
-                        jQuery('<li>')
-                        .attr( 'measure', key )
-                        .click( onMeasureClicked )
-//                         .addClass( li_class )
-                        .append(
-                            jQuery( '<a>' + dataset.getMenuItemLabel(key) + '</a>' )
-                        )
-                    );
-                } );
-}
-
-function drawTable ( indicator )
-{
-    if ( table ) {
-        // This fn is in the API docs but console says it does not exist.
-        // table.destroy( true );
-
-        jQuery('#table').empty();
-    }
-
-    jQuery('#table').append( '<table cellpadding="0" cellspacing="0" border="0" class="display"></table>' );
-
-    var is_displayable = function(x) { return x!=='' && x!==null; };
-
-    var config = {
-        data : indicator.geoNames.map( function ( geoname, index ) {
-
-            var result = [
-                geoname,
-                // ASSUMPTION: Negative data is invalid. Our current data source
-                // cannot contain nulls, hence the use of negative numbers.
-                indicator.data[index] >= 0 ? indicator.data[index] : "No valid data"
-            ];
-            return result;
-        } ),
-        columns : [
-            { title : "Area" },
-            {
-                title : indicator.title,
-                // Data may contain strings that describe missing data points.
-                // Force sorting to work for numbers rather than words.
-                type : "num"
-            }
-        ],
-        order : [[ 1, "desc" ]]
-    };
-
-    if ( indicator.data.length <= 20 ) {
-        config.paging = false;                   // Turn off paging
-        config.info = false;                     // Turn off paging info
-        config.bFilter = false;                  // Turn off search
-        config.pageLength = indicator.data.length;  // Remove empty rows
-
-        // Colour the background of wider regional rows differently.
-        config.rowCallback = function( row, data, index ) {
-            if ( ['Greater Manchester','North West','England'].indexOf( data[0] ) != -1 ) {
-                jQuery( row ).addClass( 'region' ).removeClass( 'local-authority' );
-            }
-            else {
-                jQuery( row ).removeClass( 'region' ).addClass( 'local-authority' );
-            }
-        };
-    }
-
-    table = jQuery('#table table').dataTable( config );
-}
-
 // Once the various page elements have been created, set the initial measure
 // to be viewed. This may be in the URL. If not, choose a default.
 //
 function setInitialIndicator() {
 
-    // Does the URL indicate a measure to be shown?
-    var indicator_name = getParameterFromQueryString( 'measure' );
-    if ( indicator_name ) {
+    // Does the URL say which indicator should be shown?
+    var indicator_name = getParameterFromQueryString( 'measure' ) || "";
+    if ( indicator_name && povmon_dataset.isIndicatorName(indicator_name) ) {
 
-        // TO DO:
-        // Currently we are expecting an iteration key in the query string,
-        // BUT we are more likely to get an indicator key or an indicator
-        // name. We need to add a fn to the PovmonDataset to obtain the
-        // latest iteration of a given indicator.
+        // Query string contained information about the indicator to show.
+        var indicator = povmon_dataset.indicator( indicator_name, detail_level );
 
-        var iteration_key = povmon_dataset.getLatestIndicatorKeyFromName( indicator_name, detail_level );
-        if ( iteration_key ) {
-            var indicator = povmon_dataset.indicator( iteration_key, detail_level );
-            if ( indicator ) {
-                console.debug( ["Initialising with indicator found from query string", iteration_key, indicator] );
-                selectIndicator( indicator );
-                return;
-            }
+        if ( indicator ) {
+            console.debug( ["Initialising with indicator found from query string", indicator_name, indicator] );
+            selectIndicator( indicator );
+            return;
         }
 
+        // Requested indicator has no data at this detail level.
         jQuery('#user-messages').css("display", "");
         jQuery('#user-messages li').text(
             'Information for '+indicator_name+' is not available at this detail level.'
         );
     }
 
-    // No measure indicated by URL. Use default.
-    console.debug( 'No valid indicator ("' + iteration_key + '") found from query string' );
-//     console.debug( detail_level );
-    selectIndicator( povmon_dataset.indicator(
-        povmon_dataset.latestIndicatorKeys(detail_level)[0],
-        detail_level
+    // URL did not identify an indicator.
+    console.debug( 'No loadable indicator found in query string ("' + indicator_name + '")' );
+    selectIndicator(
+        povmon_dataset.indicator(
+            povmon_dataset.availableIndicators(detail_level)[0].name, detail_level
     ));
 }
 
+function createSelector( dataset )
+{
+    dataset.availableIndicators(detail_level).forEach(
+        function (indicator) {
+            jQuery( '#measure-selector ul' )
+            .append(
+                jQuery('<li>')
+                .attr( 'measure', indicator.name )
+                .click( onMeasureClicked )
+                .append(
+                    jQuery( '<a>' + indicator.name + '</a>' )
+                )
+            );
+        }
+    );
+}
 
 // HACK??? Asynchrous creation of map and other page elements means that
 // they could become ready in either order. Set initial indicator when
@@ -426,13 +418,7 @@ function onCallbackComplete() {
 function makePageElements( dataset )
 {
     povmon_dataset = dataset;
-
     createSelector( dataset );
-
-    if ( detail_level == DetailLevel.LA ) {
-        createChart( dataset );
-    }
-
     onCallbackComplete();
 }
 
@@ -462,7 +448,7 @@ function createMap ( mapId )
     } );
 }
 
-
+// Stole this from somewhere. Probably StackOverflow.
 function getParameterFromQueryString(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -476,15 +462,14 @@ function getParameterFromQueryString(name) {
 function loadData() {
     loadPovmonDataset(
         ['indicators_geo2001_2016_04_05_a','indicators_geo2011_2016_04_05_a'],
-//         ['table_2001','table_2011'],
         'indicator_metadata_2016_04_05',
         'iteration_metadata_2016_04_05',
         makePageElements
     );
 }
 
-// And go...
 
+// And go...
 switch ( getParameterFromQueryString( 'level' ) )
 {
     case 'local-authority':
